1bef730a9161d36e192df6e35b3ea5e5dd58c5cd
Anton S <euws@gmail.com>
2019-06-03 18:59:26 +0300

    Commit 1

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,18 +2,34 @@
 namespace App\Controller;
 
 use App\Form\LoginFormType;
+use GuzzleHttp\Exception\ClientException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormError;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Attribute\AttributeBag;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\Session\Storage\NativeSessionStorage;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class LoginController extends AbstractController
 {
+    private $session;
+
+    public function __construct(SessionInterface $session)
+    {
+        $this->session = $session;
+    }
+
     /**
      * @Route("/login", name="app_login")
      */
     public function login(Request $request): Response
     {
+
+        if($this->session->get('iduser'))
+            return $this->redirect('app_homepage');
 
         $form = $this->createForm(LoginFormType::class);
         $form->handleRequest($request);
@@ -21,20 +37,62 @@ class LoginController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
 
             $client = new \GuzzleHttp\Client();
+
             $url = $this->getParameter('1cApiUrl');
             $user = $this->getParameter('1cApiUser');
             $pass = $this->getParameter('1cApiPass');
+
             $parameters = [
                 'auth' => [$user, $pass],
-                'email' => $form->get('email')->getData(),
-                'pass' => $form->get('password')->getData()
+                'Content-type' => 'application/json; charset=utf-8',
+                'Accept' => 'application/json',
+                'query' => ['login' => '1'],
+                'body' => '['.json_encode([
+                    "login" => $form->get('email')->getData(),
+                    "pass" => $form->get('password')->getData()
+                ]).']'
             ];
 
-            $response = $client->request('POST', $url, $parameters);
+            try{
+                $response = $client->request('POST', $url, $parameters);
+            } catch (ClientException $e){
+                $Error = new FormError('Ошибка сервера');
+                $form->addError($Error);
 
-            var_dump($response);exit;
+                return $this->render('login/login.html.twig', [
+                    'loginForm' => $form->createView(),
+                ]);
+            }
 
-            return $this->redirectToRoute('app_homepage');
+            $statusCode = $response->getStatusCode();
+
+            switch ($statusCode){
+                case 200:
+                    $body = $response->getBody();
+                    $remainingBytes = $body->getContents();
+                    $data = json_decode($remainingBytes);
+                    $IdUser = $data[0]->iduser;
+                    $username = $data[0]->username;
+                    $this->session = new Session(new NativeSessionStorage(), new AttributeBag());
+                    $this->session->set('iduser', $IdUser);
+                    $this->session->set('username', $username);
+
+                    return $this->redirect('/');
+                case 404:
+                    $Error = new FormError('Пользователь с данным логином или поролем не существует');
+                    $form->addError($Error);
+
+                    return $this->render('login/login.html.twig', [
+                        'loginForm' => $form->createView(),
+                    ]);
+                default:
+                    $Error = new FormError('Ошибка сервера');
+                    $form->addError($Error);
+
+                    return $this->render('login/login.html.twig', [
+                        'loginForm' => $form->createView(),
+                    ]);
+            }
         }
 
         return $this->render('login/login.html.twig', [
@@ -42,4 +100,14 @@ class LoginController extends AbstractController
         ]);
     }
 
+    /**
+     * @Route("/logout", name="app_logout")
+     */
+    public function logout()
+    {
+        if($this->session)
+            $this->session->invalidate();
+
+        return $this->redirect('/login');
+    }
 }
--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Controller;
 
+use GuzzleHttp\Exception\ClientException;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -8,7 +9,6 @@ use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class MainController extends AbstractController
 {
-
     private $session;
 
     public function __construct(SessionInterface $session)
@@ -21,12 +21,105 @@ class MainController extends AbstractController
      */
     public function index(): Response
     {
-        $foo = $this->session->get('foo');
+        $iduser = $this->session->get('iduser');
 
-        if(!$foo)
-            return $this->redirectToRoute('app_login');
+        if(!$iduser)
+            return $this->redirect('/login');
 
-        return $this->render('pages/home.html.twig');
+        $client = new \GuzzleHttp\Client();
+
+        $url = $this->getParameter('1cApiUrl');
+        $user = $this->getParameter('1cApiUser');
+        $pass = $this->getParameter('1cApiPass');
+
+        $parameters = [
+            'auth' => [$user, $pass],
+            'Content-type' => 'application/json; charset=utf-8',
+            'Accept' => 'application/json',
+            'query' => ['uauto' => '1'],
+            'body' => '['.json_encode([
+                    "iduser" => $iduser
+                ]).']'
+        ];
+
+        try{
+            $response = $client->request('POST', $url, $parameters);
+        } catch (ClientException $e){
+            return $this->render('pages/home.html.twig', ['message' => 'Ошибка сервера']);
+        }
+
+        $statusCode = $response->getStatusCode();
+
+        switch ($statusCode){
+            case 200:
+                $body = $response->getBody();
+                $remainingBytes = $body->getContents();
+                $data = json_decode($remainingBytes);
+                return $this->render('pages/home.html.twig', ['autos' => $data]);
+            case 404:
+                return $this->render('pages/home.html.twig', ['message' => 'У пользователя пока нет авто']);
+                break;
+            default:
+                return $this->render('pages/home.html.twig', ['message' => 'Ошибка сервера']);
+        }
     }
 
+    /**
+     * @Route("/auto/{idauto}", name="app_auto")
+     */
+    public function autoDetails(string $idauto): Response
+    {
+        $iduser = $this->session->get('iduser');
+
+        if(!$iduser)
+            return $this->redirect('/login');
+
+        $client = new \GuzzleHttp\Client();
+
+        $url = $this->getParameter('1cApiUrl');
+        $user = $this->getParameter('1cApiUser');
+        $pass = $this->getParameter('1cApiPass');
+
+        $parameters = [
+            'auth' => [$user, $pass],
+            'Content-type' => 'application/json; charset=utf-8',
+            'Accept' => 'application/json',
+            'query' => ['posorder' => '1'],
+            'body' => '['.json_encode([
+                    $idauto
+                ]).']'
+        ];
+
+        try{
+            $response = $client->request('POST', $url, $parameters);
+        } catch (ClientException $e){
+            return $this->render('pages/home.html.twig', ['message' => 'Ошибка сервера']);
+        }
+
+        $statusCode = $response->getStatusCode();
+
+        $body = $response->getBody();
+        $remainingBytes = $body->getContents();
+        $data = json_decode($remainingBytes);
+
+        //var_dump($data);exit;
+        $groupPart = '';
+        $dataSorted = [];
+
+        foreach ($data as $detail){
+
+            if ($detail->grouppart != $groupPart){
+                $dataSorted[$detail->grouppart] = [];
+                $dataSorted[$detail->grouppart]['id'] = uniqid();
+                $dataSorted[$detail->grouppart]['details'] = [];
+                $groupPart = $detail->grouppart;
+            }
+
+            $dataSorted[$groupPart]['details'][] =  $detail;
+        }
+
+        //var_dump( $dataSorted);exit;
+
+        return $this->render('pages/auto.html.twig', ['data' => $dataSorted]);
+    }
 }
